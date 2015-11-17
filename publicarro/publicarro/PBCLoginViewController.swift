37fc9efb8b6a6f3c61ebde35e492a854e5d4a4c2
Edilberto da Silva Ramos Júnior <edilberto.ramos@icloud.com>
2015-11-17 11:44:28 -0400

    Implementando método para feedback de conexão com internet no login.

--- publicarro/publicarro/PBCLoginViewController.swift
+++ publicarro/publicarro/PBCLoginViewController.swift
@@ -1,6 +1,7 @@
 
 import UIKit
 import Parse
+import SystemConfiguration
 
 class PBCLoginViewController: UIViewController
 {
@@ -15,6 +16,7 @@ class PBCLoginViewController: UIViewController
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: view.window)
         navigationController?.navigationBar.hidden = false
     }
+    
     
     override func didReceiveMemoryWarning()
     {
@@ -53,42 +55,52 @@ class PBCLoginViewController: UIViewController
     
     @IBAction func loginTapped(sender: AnyObject)
     {
-        
-        //se os campos estiverem validados, carrega a view de load
-        controller = storyboard!.instantiateViewControllerWithIdentifier("LoadView")
-        addChildViewController(controller!)
-        UIView.transitionWithView(view, duration: 0.0, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {self.view.addSubview(self.controller!.view)}, completion: nil)
-        
-        PFUser.logInWithUsernameInBackground(embeddedLoginViewController.emailTextField.text!, password: embeddedLoginViewController.senhaTextField.text!, block: { (user, error) -> Void in
+        if isConnectedToNetwork()
+        {
+            //se os campos estiverem validados, carrega a view de load
+            controller = storyboard!.instantiateViewControllerWithIdentifier("LoadView")
+            addChildViewController(controller!)
+            UIView.transitionWithView(view, duration: 0.0, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {self.view.addSubview(self.controller!.view)}, completion: nil)
             
-            
-            if user != nil
-            {
-                //caso tenha conseguido salvar com sucesso, para de exibir a view de load.
-                self.controller!.view.removeFromSuperview()
-
-                print("Usuário Logado")
-            }
-            else
-            {
-                //caso tenha conseguido salvar com sucesso, para de exibir a view de load.
-                self.controller!.view.removeFromSuperview()
-
-                print(error)
-                switch(error?.code)
+            PFUser.logInWithUsernameInBackground(embeddedLoginViewController.emailTextField.text!, password: embeddedLoginViewController.senhaTextField.text!, block: { (user, error) -> Void in
+                
+                
+                if user != nil
                 {
-                    case 101?:
-                        let alertView = UIAlertController(title: "Aviso", message: "Email ou senha incorretos.", preferredStyle: .Alert)
-                        self.presentViewController(alertView, animated: false, completion: nil)
-                        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1.5*Double(NSEC_PER_SEC))),dispatch_get_main_queue(),{
-                            alertView.dismissViewControllerAnimated(false, completion: nil)
-                        })
-                    break
-                    default:
-                    break
+                    //caso tenha conseguido salvar com sucesso, para de exibir a view de load.
+                    self.controller!.view.removeFromSuperview()
+
+                    print("Usuário Logado")
                 }
-            }
-        })
+                else
+                {
+                    //caso tenha conseguido salvar com sucesso, para de exibir a view de load.
+                    self.controller!.view.removeFromSuperview()
+
+                    print(error)
+                    switch(error?.code)
+                    {
+                        case 101?:
+                            let alertView = UIAlertController(title: "Aviso", message: "Email ou senha incorretos.", preferredStyle: .Alert)
+                            self.presentViewController(alertView, animated: false, completion: nil)
+                            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1.5*Double(NSEC_PER_SEC))),dispatch_get_main_queue(),{
+                                alertView.dismissViewControllerAnimated(false, completion: nil)
+                            })
+                        break
+                        default:
+                        break
+                    }
+                }
+            })
+        }else{
+            let alertView = UIAlertController(title: "Aviso", message: "Sem conexão com a internet", preferredStyle: .Alert)
+            self.presentViewController(alertView, animated: false, completion: nil)
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1.5*Double(NSEC_PER_SEC))),dispatch_get_main_queue(),{
+                alertView.dismissViewControllerAnimated(false, completion: nil)
+            })
+
+            
+        }
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
@@ -97,5 +109,22 @@ class PBCLoginViewController: UIViewController
         {
             embeddedLoginViewController = segue.destinationViewController as? PBCLoginTableViewController
         }
+    }
+    
+    // Conexão com a internet
+    func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
     }
 }
