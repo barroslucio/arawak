705c800bc09cd7a3af06a08534ada69f68f9ab4e
Edilberto da Silva Ramos Júnior <edilberto.ramos@icloud.com>
2015-11-24 08:35:27 -0400

    Criando classe tableviewcell para exibir anúncios.

---
+++ publicarro/publicarro/AnuncioDisponivelTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnuncioDisponivelTableViewCell.swift
+//  publiCarro
+//
+//  Created by EDILBERTO DA SILVA RAMOS JUNIOR on 24/11/15.
+//  Copyright © 2015 tambatech. All rights reserved.
+//
+
+import UIKit
+
+class AnuncioDisponivelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
