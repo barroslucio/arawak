45f246dbf3e7cb9449f74824c44f161e832aac20
Filipe da Silva Oliveira <fdso@outlook.com>
2015-11-27 12:01:40 -0400

    Transição Login

--- publicarro/publicarro/PBCLoginViewController.swift
+++ publicarro/publicarro/PBCLoginViewController.swift
@@ -5,38 +5,24 @@ import SystemConfiguration
 
 class PBCLoginViewController: UIViewController
 {
-    
-    
-    //variavel que vai receber a view de load
+    @IBOutlet var bottonConstraint: NSLayoutConstraint!
     
     var array = NSArray()
-    
-    var controller: PBCLoadAnimationViewController!
 
-    @IBOutlet var bottonConstraint: NSLayoutConstraint!
-    private var embeddedLoginViewController : PBCLoginTableViewController!
+    private var embeddedLoginViewController: PBCLoginTableViewController!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name: UIKeyboardWillShowNotification, object: view.window)
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name: UIKeyboardWillHideNotification, object: view.window)
-        
-        let dismiss: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
-        view.addGestureRecognizer(dismiss)
-        
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: "dismissKeyboard"))
         navigationController?.navigationBar.hidden = false
     }
     
-    
-    
-    func DismissKeyboard(){
-        view.endEditing(true)
-    }
-    
-    override func didReceiveMemoryWarning()
+    func DismissKeyboard()
     {
-        super.didReceiveMemoryWarning()
+        view.endEditing(true)
     }
     
     func keyboardWillShow(notification: NSNotification)
@@ -68,96 +54,80 @@ class PBCLoginViewController: UIViewController
     
     @IBAction func loginTapped(sender: AnyObject)
     {
-        
-    
-      
-        
+        var controller: PBCLoadAnimationViewController
         if isConnectedToNetwork()
         {
-            //se os campos estiverem validados, carrega a view de load
-            self.controller = storyboard!.instantiateViewControllerWithIdentifier("LoadView") as! PBCLoadAnimationViewController
-            addChildViewController(self.controller!)
-            UIView.transitionWithView(view, duration: 0.0, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
-                
-                self.view.addSubview(self.controller!.view)
-                self.controller.infoLabel.text = "Realizando login..."
-                
-                }, completion: nil)
-
+            controller = storyboard!.instantiateViewControllerWithIdentifier("LoadView") as! PBCLoadAnimationViewController
+            addChildViewController(controller)
+            UIView.transitionWithView(view, duration: 0.0, options: UIViewAnimationOptions.TransitionCrossDissolve, animations:
+            {
+                self.view.addSubview(controller.view)
+                controller.infoLabel.text = "Realizando login..."
+            }, completion: nil)
                 PFUser.logInWithUsernameInBackground(embeddedLoginViewController.emailTextField.text!, password: embeddedLoginViewController.senhaTextField.text!, block: { (user, error) -> Void in
-                    
-                    
-                    
-                    
                 if user != nil
                 {
                     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1.0*Double(NSEC_PER_SEC))),dispatch_get_main_queue(),
-                        { self.controller.view.removeFromSuperview()})
-                    print("Usuário Logado")
-                    
-                    let controller = self.storyboard!.instantiateViewControllerWithIdentifier("AnunciosTabBar")
-                    self.presentViewController(controller, animated: true, completion: nil)
-
-                    
+                        { controller.view.removeFromSuperview()
+                            self.presentViewController(self.storyboard!.instantiateViewControllerWithIdentifier("AnunciosTabBar"), animated: true, completion: nil)
+                    })
                 }
                 else
                 {
-                    var mensagem = String()
+                    var mensagem: String!
                     switch(error?.code)
                     {
-                    case 101?:
+                        case 101?:
                         mensagem = "Email ou senha incorretos."
                         break
-                    default:
+                        default:
                         mensagem = "[ALGUM ERRO]"
                         break
                     }
-                    //se os campos estiverem validados, carrega a view de load
-                    self.controller.falha()
-                    self.controller.infoLabel.text = mensagem
-
-                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1*Double(NSEC_PER_SEC))),dispatch_get_main_queue(),{
-                        self.controller.view.removeFromSuperview()
+                    controller.infoLabel.text = mensagem
+                    controller.falha()
+                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.7*Double(NSEC_PER_SEC))),dispatch_get_main_queue(),{
+                        controller.view.removeFromSuperview()
                     })
                 }
             })
-        } else {
-            //se os campos estiverem validados, carrega a view de load
-            self.controller = storyboard!.instantiateViewControllerWithIdentifier("LoadView") as! PBCLoadAnimationViewController
-            addChildViewController(self.controller!)
-            UIView.transitionWithView(view, duration: 0.0, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: {
-                
-                self.view.addSubview(self.controller!.view)
-                self.controller.falha()
-                self.controller.infoLabel.text = "Sem conexão."
-                
-                }, completion: nil)
-            
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(1*Double(NSEC_PER_SEC))),dispatch_get_main_queue(),{
-                self.controller.view.removeFromSuperview()
+        }
+        else
+        {
+            controller = storyboard!.instantiateViewControllerWithIdentifier("LoadView") as! PBCLoadAnimationViewController
+            addChildViewController(controller)
+            UIView.transitionWithView(view, duration: 0.0, options: UIViewAnimationOptions.TransitionCrossDissolve, animations:
+            {
+                self.view.addSubview(controller.view)
+                controller.infoLabel.text = "Sem conexão."
+                controller.falha()
+            }, completion: nil)
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(0.7*Double(NSEC_PER_SEC))),dispatch_get_main_queue(),{
+                controller.view.removeFromSuperview()
             })
         }
     }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
     {
-        //segue para a table view controller
         if(segue.identifier == "LoginEmbedSegue")
         {
             embeddedLoginViewController = segue.destinationViewController as? PBCLoginTableViewController
         }
     }
     
-    // Conexão com a internet
-    func isConnectedToNetwork() -> Bool {
+    func isConnectedToNetwork() -> Bool
+    {
         var zeroAddress = sockaddr_in()
         zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
         zeroAddress.sin_family = sa_family_t(AF_INET)
-        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress)
+        {
             SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
         }
         var flags = SCNetworkReachabilityFlags()
-        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags)
+        {
             return false
         }
         let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
